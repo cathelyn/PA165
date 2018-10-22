131bb645289e699e30a9a2ea1ed9e76dca51d89b
Katarina Bulkova <bulkova.katarina@gmail.com>
2018-10-22 07:42:02 +0200

    task 03

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -33,4 +34,8 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		
+	}
 }
